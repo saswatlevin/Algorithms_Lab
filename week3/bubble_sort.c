06b4c813b7daf1bf99581ef3656ec804ad29dce4
Sauravp98 <sauravp98@gmail.com>
2018-10-05 03:07:26 +0530

    i have structured the bubblesort code

--- week3/bubble_sort.c
+++ week3/bubble_sort.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define MAX_CAPACITY 1000
-
 int count=0;
-void swap(int *xi,int *xj){
-
-int temp=0;
-temp=*xi;
-*xi=*xj;
-*xj=temp;
-++count;
-
+void swap(int *xi,int *xj)
+{
+    int temp=0;
+    temp=*xi;
+    *xi=*xj;
+    *xj=temp;
+    ++count;
 }
-
-void bubble_sort(int size,int A[MAX_CAPACITY]){
- int i=0,j=0,count=0;
- for(i=0;i<size-1;++i){
-  
-  for(j=0;j<size-i-1;++j){
-      ++count;
-      if(A[j]>A[j+1]){
-      
-      swap(&A[j],&A[j+1]);  
-    
-    }
-      
+void bubble_sort(int n,int A[MAX_CAPACITY])
+{
+    int i=0,j=0,count=0;
+    for(i=0;i<n-1;++i)
+     {
+       for(j=0;j<n-i-1;++j)
+       {
+           ++count;
+          if(A[j]>A[j+1])
+          {
+            swap(&A[j],&A[j+1]);  
+          }   
+       }
   }
-
-    }
-
 }
-
-
 void list_input(int size,int A[MAX_CAPACITY])
 {
   int i=0;
@@ -42,32 +34,29 @@ void list_input(int size,int A[MAX_CAPACITY])
     scanf("%d",&A[i]);
   } 
 }
-
-void print_sorted(int A[MAX_CAPACITY],int size)
+void print_sorted(int A[MAX_CAPACITY],int n)
 {
    int i=0;
-   if(size>MAX_CAPACITY)
+   if(n>MAX_CAPACITY)
    {
     printf("\nList Size exceeding Maximum Capacity.Exiting...");
         exit(1);
    }
-   
-   for(i=0;i<size;++i)
-   {
-      
+   for(i=0;i<n;i++)
+   {  
       printf("%d\t",A[i]);
    }
-
 }
 
 int main(int argc,const char *argv[])
 {
-  int i=0,j=0,size=0,A[MAX_CAPACITY];
+  int n;
   printf("\nEnter the size of the list\t");
-  scanf("%d",&size);
-  list_input(size,A);
-  bubble_sort(size,A);
-  print_sorted(A,size);
+  scanf("%d",&n);
+  int A[n];
+  list_input(n,A);
+  bubble_sort(n,A);
+  print_sorted(A,n);
   printf("\nNumber of times swap occurred is= %d\n",count);
   printf("\nNumber of comparisons=%d",count);
   return 0;
